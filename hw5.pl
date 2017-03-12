74863833de79272f422883a9569539f4007a59e0
wonhee <wonheeprk@gmail.com>
2017-03-12 03:48:27 -0700

    part1 half the test cases work

--- hw5.pl
+++ hw5.pl
@@ -3,6 +3,8 @@
 /*******************************************/
 
 
+/***************** PART 1 ******************/
+
 /* Given Set of facts: 			 */
 /* course(number, prereq, units) */
 /* student(name, courses taken)  */
@@ -11,15 +13,17 @@
 /* courses with 3 or 4 units */
 fc_course(C) :- 
 	course(C,_,X), 
-	(A=3; A=4).
+	(X=3; X=4).
 
 /* courses whose immediate pre-req is ecs 110 */
 prereq_110(P) :- 
-	course(P, ecs110, _).
+	course(P,X,_),
+	member(ecs110,X).
 
 /* names of all students in ecs140a */
 ecs140a_students(S) :-
-	student(S, ecs140a).
+	student(S,X),
+	member(ecs140a,X).
 
 /* name of all instructors who teach john's courses */
 instructor_names(I) :-
@@ -29,8 +33,14 @@ instructor_names(I) :-
 /* names of all students who are in jim's class */
 students(S) :-
 	student(S, X),
-	instructor(jim, X).
+	instructor(N, X),
+	member(jim, N).
 
 /* all pre-req of course */
 allprereq(C, A) :-
 	course(C, A, _).
+
+/***************** PART 2 ******************/
+
+
+
