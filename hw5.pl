813428fd264cec83c9022f106caf7d57bfd5112f
wonhee <wonheeprk@gmail.com>
2017-03-14 03:47:32 -0700

    fix merges

--- hw5.pl
+++ hw5.pl
@@ -30,6 +30,7 @@ instructor_names(I) :-
 	instructor(I, IC),
 	student(john, SC),
 	intersection(SC, IC).
+	/* I think this works because it separates the intersection logic away from the original and statement */
 
 /* names of all students who are in jim's class */
 students(S):-
@@ -43,29 +44,36 @@ intersection(SC, IC):-
 	member(X, IC),
 	!.
 
-/* all pre-req of course, not working */
-allprereq([],[]) :- !.
-allprereq([],A).
-allprereq([H|T],A) :-
-	course(H,X,_), 
-	atomic_list_concat(L,'',H),  
-	append(X,L,A),
-	allprereq(X,A).
+/* all pre-req of course */
+allprereq([],Prereq):-
+	Prereq = [].
+allprereq([H|T], Prereq):-
+  /* Continue recursion on head */
+	course(H, HeadPrereq, _),
+	allprereq(HeadPrereq, HeadPrereqPrereq),
+	/* Continue recursion on tail */
+	allprereq(T, TailPrereq),
+	append(HeadPrereqPrereq, HeadPrereq, HeadTotalPrereq),
+	append(TailPrereq, HeadTotalPrereq, Prereq).
+allprereq(Course, Prereq):-
+	course(Course, CoursePrereqs, _),
+	allprereq(CoursePrereqs, CoursePrereqsPrereqs),
+	append(CoursePrereqsPrereqs, CoursePrereqs, Prereq).
 
 /***************** PART 2 ******************/
 
-/* takes a list and counts number of atoms that occur in the list at all levels, not working */
-all_length([],0). 
-all_length([H|T],L) :-
-	atom(H),
-	all_length(T,L1),
-	L is L1+1,
-	!.
-all_length([H|T],L) :-
-	is_list(H),
-	all_length(H,L1),
-	all_length(T,L2),
-	L is L1+L2.
+/* takes a list and counts number of atoms that occur in the list at all levels */
+all_length([],0).
+all_length([H|T],Result):-
+	all_length(T, ResultOfTail),
+	(is_list(H) ->
+		(length(H, 0) ->
+			ResultOfHead = 1;
+			all_length(H, ResultOfHead)
+			);
+		ResultOfHead=1
+		),
+	Result is ResultOfTail + ResultOfHead.
 
 /* returns true if L contains equal number of a and b terms, not working */
 equal_a_b(L) :- 
@@ -79,34 +87,6 @@ compare([H|T], A, B) :-
 	;(B = 1)),
 	compare(T, A, B).
 
-all_length([],0).
-all_length([H|T],Result):-
-	all_length(T, ResultOfTail),
-	(is_list(H) ->
-		(length(H, 0) ->
-			ResultOfHead = 1;
-			all_length(H, ResultOfHead)
-			);
-		ResultOfHead=1
-		),
-	Result is ResultOfTail + ResultOfHead.
-
-/* swap prefix and suffix of list */
-/* K is a sublist of L */
-/* S is list obtained by appending suffix of L, following occurrence of K in L, followed by prefix of L until K */
-swap_prefix_suffix(K,L,S) :-
-!
-.
-
-/* returns true if list A is palindrome */
-palin(A) :-
-!
-	.
-
-/* returns true if A is good sequence: either a single number 0 or number 1 followed by 2 other good sequences */
-good(A) :-
-!
-	.
 
 
 
